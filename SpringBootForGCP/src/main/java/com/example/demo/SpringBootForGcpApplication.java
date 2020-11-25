c31c9e45bee632d9f6991dfb0a59bdaf35665211
Kirankumar Shejwal <kirankumar.shejwal@accentue.com>
2020-11-25 23:14:01 +0530

    first commit

---
+++ SpringBootForGCP/src/main/java/com/example/demo/SpringBootForGcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForGcpApplication.class, args);
+	}
+
+}
